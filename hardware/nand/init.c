5cb30f307d6ca560568f0740544177031e1c2871
baron <baron@163.com>
2013-05-28 21:37:31 +0800

    nand
    Signed-off-by: baron <baron@163.com>

--- hardware/nand/init.c
+++ hardware/nand/init.c
@@ -1,11 +1,6 @@
-/* WOTCH DOG register */
 #define 	WTCON				(*(volatile unsigned long *)0x53000000)
-
-/* SDRAM regisers */
 #define 	MEM_CTL_BASE		0x48000000
 #define 	SDRAM_BASE		0x30000000
-
-/* NAND Flash registers */
 #define NFCONF			(*(volatile unsigned long *)0x4E000000)
 #define NFCMMD			(*(volatile unsigned char *)0x4E000008)
 #define NFADDR			(*(volatile unsigned char *)0x4E00000C)
@@ -13,25 +8,21 @@
 #define NFSTAT			(*(volatile unsigned char *)0x4E000020)
 #define NFCONT  		(*(volatile unsigned long *)0x4E000004)
 
-
-/* SDRAM 13个寄存器的值 */
 unsigned long  const	mem_cfg_val[]={	0x22111110,		//BWSCON
-									0x00000700,		//BANKCON0
-									0x00000700,		//BANKCON1
-									0x00000700,		//BANKCON2
-									0x00000700,		//BANKCON3
-									0x00000700,		//BANKCON4
-									0x00000700,		//BANKCON5
-									0x00018005,		//BANKCON6
-									0x00018005,		//BANKCON7
-									0x008e07a3,		//REFRESH
-									0x000000b2,		//BANKSIZE
-									0x00000030,		//MRSRB6
-									0x00000030,		//MRSRB7
-							   	    };
+	0x00000700,		//BANKCON0
+	0x00000700,		//BANKCON1
+	0x00000700,		//BANKCON2
+	0x00000700,		//BANKCON3
+	0x00000700,		//BANKCON4
+	0x00000700,		//BANKCON5
+	0x00018005,		//BANKCON6
+	0x00018005,		//BANKCON7
+	0x008e07a3,		//REFRESH
+	0x000000b2,		//BANKSIZE
+	0x00000030,		//MRSRB6
+	0x00000030,		//MRSRB7
+};
 
-
- 
 void disable_watch_dog();
 void memsetup();
 void reset_nand();
@@ -39,14 +30,10 @@ void init_nand();
 inline void wait_idle(void);
 void nand_read_ll(unsigned char *buf, unsigned long start_addr, int size);
 
-
-/*上电后，WATCH DOG默认是开着的，要把它关掉 */
 void disable_watch_dog()
 {
 	WTCON	= 0;
 }
-
-/* 设置控制SDRAM的13个寄存器 */
 void memsetup()
 {
 	int 	i = 0;
@@ -55,17 +42,15 @@ void memsetup()
 		p[i] = mem_cfg_val[i];
 }
 
-/* 在第一次实用NAND Flash前，复位一下NAND Flash */
 void reset_nand()
 {
 	int i=0;
 	NFCONF &= ~0x300;
-    	for(; i<10; i++);
+	for(; i<10; i++);
 	NFCMMD = 0xFF;	//reset command
 	wait_idle();
 }
 
-/* 初始化NAND Flash */
 void init_nand()
 {
 	NFCONF = 0xf830;
@@ -74,10 +59,10 @@ void init_nand()
 
 #define BUSY 1
 inline void wait_idle(void) {
-    int i;
+	int i;
 
-    while(!(NFSTAT & BUSY))
-      for(i=0; i<10; i++);
+	while(!(NFSTAT & BUSY))
+		for(i=0; i<10; i++);
 }
 
 #define NAND_SECTOR_SIZE	512
@@ -86,37 +71,36 @@ inline void wait_idle(void) {
 /* low level nand read function */
 void nand_read_ll(unsigned char *buf, unsigned long start_addr, int size)
 {
-    int i, j;
+	int i, j;
 
-    if ((start_addr & NAND_BLOCK_MASK) || (size & NAND_BLOCK_MASK)) {
-        return ;	/* invalid alignment */
-    }
+	if ((start_addr & NAND_BLOCK_MASK) || (size & NAND_BLOCK_MASK)) {
+		return ;	/* invalid alignment */
+	}
 
-    /* chip Enable */
-    NFCONF = (3<<8);
-    NFCONT = 1;
-    for(i=0; i<10; i++);
+	/* chip Enable */
+	NFCONF = (3<<8);
+	NFCONT = 1;
+	for(i=0; i<10; i++);
 
-    for(i=start_addr; i < (start_addr + size);) {
-      /* READ0 */
-      NFCMMD = 00;
+	for(i=start_addr; i < (start_addr + size);) {
+		/* READ0 */
+		NFCMMD = 00;
 
-      /* Write Address */
-      NFADDR = i & 0xff;
-      NFADDR = (i >> 9) & 0xff;
-      NFADDR = (i >> 17) & 0xff;
-      NFADDR = (i >> 25) & 0xff;
+		/* Write Address */
+		NFADDR = i & 0xff;
+		NFADDR = (i >> 9) & 0xff;
+		NFADDR = (i >> 17) & 0xff;
+		NFADDR = (i >> 25) & 0xff;
 
-      wait_idle();
+		wait_idle();
 
-      for(j=0; j < NAND_SECTOR_SIZE; j++, i++) {
-	*buf = (NFDATA & 0xff);
-	buf++;
-      }
-    }
+		for(j=0; j < NAND_SECTOR_SIZE; j++, i++) {
+			*buf = (NFDATA & 0xff);
+			buf++;
+		}
+	}
 
-    /* chip Disable */
-    NFCONT |= 0x02;	/* chip disable */
+		NFCONT |= 0x02;	/* chip disable */
 
-    return ;
+	return ;
 }
